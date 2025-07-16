e4890bd1c22182c6d98bce68beb0ba1968e4e0f1
prabhat0204 <prabhat0204@gmail.com>
2025-07-16 19:10:31 +0530

    Create db.php

---
+++ db.php
@@ -0,0 +1,26 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $dbname = "your_database";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->host};dbname={$this->dbname}",
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "Database connection error: " . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
